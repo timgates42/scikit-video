98a8d5644b9eb1cee0689bc4fa243a06c4a9ff18
Tim Gates <tim.gates@iress.com>
2020-02-28 18:28:29 +1100

    Fix simple typo: framerwork -> framework
    Closes #7

--- skvideo/__init__.py
+++ skvideo/__init__.py
@@ -1,6 +1,6 @@
 from skvideo.version import __version__
 
-# If you want to use Numpy's testing framerwork, use the following.
+# If you want to use Numpy's testing framework, use the following.
 # Tests go under directory tests/, benchmarks under directory benchmarks/
 #from numpy.testing import Tester
 #test = Tester().test
